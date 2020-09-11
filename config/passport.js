ede46dd1d8415bceec7aa7902b3ff73d4ca51130
praytop <praytop@gmail.com>
2020-09-11 09:37:30 +0900

    Board 에 데이터 없는경우 타이틀 잘리는 문제 해결

--- config/passport.js
+++ config/passport.js
@@ -4,18 +4,21 @@ var User = require("../models/User");
 
 // serialize & deserialize User
 passport.serializeUser(function (user, done) {
-  done(null, user.id);
+  // Strategy 성공 시 호출
+  done(null, user.id); // 여기의 user.id가 deserializeUser의 첫 번째 매개변수로 이동
 });
 passport.deserializeUser(function (id, done) {
+  // 매개변수 id 는 serializeUser 의 done 의 인자 user.id 를 받은 것.
   User.findOne({ _id: id }, function (err, user) {
-    done(err, user);
+    // id 로 User 정보를 찾는다. user정보를 바로 넣어도 되지만 DB 에서 변경된 내용을 가져오기 위해 id를 이용 조회 처리.
+    done(err, user); // 여기의 user 가 req.user 가 됨.
   });
 });
 
 // local strategy
 passport.use(
   "local-login",
-  new LocalStrategy(
+  new LocalStrategy( // local 전략을 세움.
     {
       usernameField: "username",
       passwordField: "password",
