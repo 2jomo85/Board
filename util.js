da57d9328538392c480a12dbb7c6df4f065f6986
praytop <praytop@gmail.com>
2020-09-11 15:39:09 +0900

    접근제한 기능 추가.

--- util.js
+++ util.js
@@ -1,19 +1,34 @@
 var util = {};
 
 // functions
-util.parseError = function (errors) {
-  var parsed = {};
-  if (errors.name == "ValidationError") {
-    for (var name in errors.errors) {
-      var validationError = errors.errors[name];
-      parsed[name] = { message: validationError.message };
+util.parseError = function(errors) {
+    var parsed = {};
+    if (errors.name == "ValidationError") {
+        for (var name in errors.errors) {
+            var validationError = errors.errors[name];
+            parsed[name] = { message: validationError.message };
+        }
+    } else if (errors.code == "11000" && errors.errmsg.indexof("username") > 0) {
+        parsed.username = { message: "This username already exists!" };
+    } else {
+        parsed.unhandled = JSON.stringify(errors);
     }
-  } else if (errors.code == "11000" && errors.errmsg.indexof("username") > 0) {
-    parsed.username = { message: "This username already exists!" };
-  } else {
-    parsed.unhandled = JSON.stringify(errors);
-  }
-  return parsed;
+    return parsed;
 };
+
+util.isLoggedin = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        req.flash('errors', { login: 'Pleas login first' });
+        res.redirect('/login');
+    }
+}
+
+util.noPermission = function(req, res) {
+    req.flash('errors', { login: "You don't have permossion" });
+    req.logout();
+    res.redirect('/login');
+}
 
 module.exports = util;
